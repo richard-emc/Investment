63084029e6851aed2f52e7a918d222f33dc3f7f3
richard-emc <120187029+richard-emc@users.noreply.github.com>
2025-06-26 19:34:04 +0000

    hello world

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main (){
-
+    int a;
     cout << "Hello, World!" << endl;
+    cout << "This is a C++17 project." << endl;
+    cout << "Please enter an integer: ";
+    cin>>a;
+    cout << "You entered: " << a << endl;
+    cout << "Press Enter to exit..." << endl;
     return 0;
 };
